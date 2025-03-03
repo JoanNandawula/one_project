9a3384a9d7a9542912b3d173c8cf31b2f63e624d
NandawulaJoan <jnandawula77@gmail.com>
2025-03-04 02:33:10 +0300

    sec commit

--- daystar/settings.py
+++ daystar/settings.py
@@ -12,9 +12,6 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 import os
 from pathlib import Path
 
-# Existing BASE_DIR definition is likely using Path:
-BASE_DIR = Path(__file__).resolve().parent.parent
-from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
